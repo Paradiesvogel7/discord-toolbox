456b3b0eb16a390810f9c52d876e1b4877f16b0c
Yoel-B <bider.yoel@gmail.com>
2024-04-12 18:02:20 +0200

    footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Footer: React.FC = () => {
+  return (
+    <footer>
+      <div className="z-30 ml-8 mb-8 text-small text-white flex gap-4">
+        © {new Date().getFullYear()}. All rights reserved
+      </div>
+    </footer>
+  );
+};
